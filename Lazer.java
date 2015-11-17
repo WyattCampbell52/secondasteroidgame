8e6ac8ce4325be986dab1668c9e1f6cb7b305ee0
WyattCampbell52 <WyattCampbell@Wyatts-MacBook-Air.local>
2015-11-16 21:03:09 -0800

    Started Projectiles

---
+++ Lazer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secondasteroidgame;
+
+import environment.Actor;
+import environment.Velocity;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author WyattCampbell
+ */
+public class Lazer extends Actor {
+
+    public Lazer(BufferedImage image, Point position, Velocity velocity) {
+        super(image, position, velocity);
+    }
+    
+    
+    
+}
--- Space.java
+++ Space.java
@@ -26,16 +26,22 @@ class Space extends Environment {
     Image ship;
     Image background;
     Image fullAsteroid;
+    Image lazer;
     Velocity velocity;
     Velocity asteroidVelocity;
     String name;
+    Velocity lazerVelocity;
     int angle = 0;
     int shipX = 400;
     int shipY = 300;
     int rotationSpeed = 5;
     int shipSpeed = 5;
+    int lazerSpeed = 15;
+    int lazerX = 400;
+    int lazerY = 300;
     int asteroidX = 100;
     int asteroidY = 100;
+    
     
     
     private double getAngleInRadians(){
@@ -44,9 +50,11 @@ class Space extends Environment {
 
     public Space() {
         this.setBackground(background);
+        lazerVelocity = new Velocity(0,0);
         velocity = new Velocity(0, 0);
         asteroidVelocity = new Velocity(3,4);
         name = JOptionPane.showInputDialog("What Ship? American or Soviet");
+        lazer = ResourceTools.loadImageFromResource("SecondAsteroidGame/Lazer.png");
         fullAsteroid = ResourceTools.loadImageFromResource("SecondAsteroidGame/Full Asteroid.png");
         ship = ResourceTools.loadImageFromResource("SecondAsteroidGame/" + name +" Ship.png");
         background = ResourceTools.loadImageFromResource("SecondAsteroidGame/Galaxy 1.jpg");
@@ -58,6 +66,8 @@ class Space extends Environment {
 
     @Override
     public void timerTaskHandler() {
+        this.lazerY -= lazerVelocity.y;
+        this.lazerX -= lazerVelocity.x;
         this.shipX -= velocity.x;
         this.shipY -= velocity.y;
         this.asteroidX -= asteroidVelocity.x;
@@ -66,19 +76,30 @@ class Space extends Environment {
         asteroidX++;
         
         if (shipX < -50) {
-            shipX = this.getWidth() - 1;
-        } else if (shipX > this.getWidth() + 50) {
+            shipX = 900;
+        } else if (shipX > 900) {
             shipX = -50;
         }
         
-        if (shipY < -50) {
-            shipY = this.getHeight() - 1;
-        } else if (shipY > this.getHeight() + 50) {
-            shipY = -50;
+        if (shipY < -100) {
+            shipY = 550;
+        } else if (shipY > 550) {
+            shipY = -100;
+        }
+        if (lazerX < -50) {
+            lazerX = 900;
+        } else if (lazerX > 900) {
+            lazerX = -50;
+        }
+        
+        if (lazerY < -50) {
+            lazerY = 600;
+        } else if (lazerY > 600) {
+            lazerY = -50;
         }
         if (asteroidX < -100) {
-            asteroidX = 500;
-        } else if (asteroidX > 500) {
+            asteroidX = 600;
+        } else if (asteroidX > 600) {
             asteroidX = -100;
         }
         
@@ -87,6 +108,7 @@ class Space extends Environment {
         } else if (asteroidY > 1000) {
             asteroidY = -75;
         }
+        
         
     }
 
@@ -98,11 +120,11 @@ class Space extends Environment {
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             angle = ((angle - rotationSpeed) % 360);
         } else if (e.getKeyCode() == KeyEvent.VK_UP) {
-            shipY = (shipY - 5);
-        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            shipY = (shipY + 5);
-        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            this.lazerVelocity = TrigonometryCalculator.getVelocity(Math.toRadians((angle + 90) % 360), shipSpeed);
             this.velocity = TrigonometryCalculator.getVelocity(Math.toRadians((angle + 90) % 360), shipSpeed);
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            this.lazerVelocity = TrigonometryCalculator.getVelocity(Math.toRadians((angle + 90) % 360), lazerSpeed);
+
 
         }
         System.out.println("Angle " + angle);
@@ -126,16 +148,23 @@ class Space extends Environment {
             graphics.drawImage(background, WIDTH, HEIGHT, this);
         }
         if (fullAsteroid != null) {
+            AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(angle));
+            at.setToRotation(getAngleInRadians(), lazerX , lazerY);
+
             graphics.drawImage(fullAsteroid, asteroidY, asteroidX, this);
         }
-        
+        if (lazer != null) {
+            AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(angle));
+            at.setToRotation(getAngleInRadians(), lazerX , lazerY);
+            g2d.setTransform(at);
+            g2d.drawImage(lazer, lazerX, lazerY, this);
+        }
         if (ship != null) {
             AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(angle));
             at.setToRotation(getAngleInRadians(), shipX + (ship.getWidth(this)/ 2), shipY + (ship.getHeight(this) / 2));
             g2d.setTransform(at);
             g2d.drawImage(ship, shipX, shipY, this);
         }
- 
         
         g2d.setTransform(olde);
         g2d.dispose();
